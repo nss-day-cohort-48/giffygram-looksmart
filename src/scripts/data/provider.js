07adc2eaa85316f127e1ce4508f63f60618fdab5
wooldridge12 <wooldridgemikey@gmail.com>
2021-05-19 16:53:59 -0400

    fetchUsers defined

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -8,5 +8,22 @@ const applicationState = {
         chosenUser: null,
         displayFavorites: false,
         displayMessages: false
-    }
+    },
+    users: []
+}
+
+
+export const fetchUsers = () => {
+    return fetch(`${apiURL}/users`)
+    .then(response => response.json())
+    .then(
+        (user) => {
+            applicationState.users = user
+        }
+    )
+}
+
+
+export const getUsers = () => {
+    return [...applicationState.users]
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,11 @@
 import { GiffyGram } from "./GiffyGram.js"
+import { LoginForm } from "./auth/Login.js"
 
 const applicationElement = document.querySelector(".giffygram")
 
 export const renderApp = () => {
     const user = parseInt(localStorage.getItem("gg_user"))
-
+    
     if (user) {
         applicationElement.innerHTML = GiffyGram()
     } else {
