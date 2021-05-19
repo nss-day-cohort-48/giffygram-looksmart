c835f77728951118476da814de8f03750eeb6a22
alambrow <alexander.lambrow@gmail.com>
2021-05-19 15:55:27 -0500

    fetchPosts function defined

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -2,11 +2,32 @@ const apiURL = "http://localhost:8088"
 const applicationElement = document.querySelector(".giffygram")
 
 
-const applicationState = {
+let applicationState = {
     currentUser: {},
     feed: {
         chosenUser: null,
         displayFavorites: false,
         displayMessages: false
-    }
+    },
+    users: [],
+    posts: [],
+    likes: [],
+    messages: [],
+    follows: []
+}
+
+
+
+
+
+export const fetchPosts = () => {
+    return fetch("${API}/posts")
+    .then(response => response.json())
+    .then(posts =>{
+        applicationState.posts = posts
+    })
+}
+
+export const getPosts = () => {
+    return [...applicationState.posts]
 }
