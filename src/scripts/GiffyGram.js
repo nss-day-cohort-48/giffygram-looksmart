d0d089e8a0675b55b01840da3145b0d9334df90f
Sean Murphey <smurphey1990@gmail.com>
2021-05-20 09:10:22 -0500

    Added NavBar.js module and outlined HTML for GiffyGram.js

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,5 +1,26 @@
 export const GiffyGram = () => {
 
     // Show main main UI
-    return `<h1>Giffygram</h1>`
+    return `<h1>Giffygram</h1>
+    
+    <section class="Header">
+
+    <div class="DirectMessage"></div>
+    
+    </section>
+
+    <section class="MessageForm">
+    
+    </section>
+
+    <section class="PostList">
+
+    </section>
+
+    <section class="Footer">
+    
+    </section>
+
+
+    `
 }
