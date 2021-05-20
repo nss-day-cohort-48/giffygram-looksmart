c6d9611ab2e39c907a14d257723f26d59999c4bb
alambrow <alexander.lambrow@gmail.com>
2021-05-20 13:09:06 -0500

    implemented user registration component

--- src/scripts/auth/Register.js
+++ src/scripts/auth/Register.js
@@ -1,4 +1,4 @@
-
+import { getUsers, sendUserToDatabase } from "../data/provider.js"
 
 export const registrationForm = () => {
     return `
@@ -6,7 +6,9 @@ export const registrationForm = () => {
             <div>
             Not a member?
             </div>
-        <button id="registrationButton>Register here!</button>
+
+            <button id="registrationButton>Register here!</button>
+
         </div>
     `
 }
@@ -21,5 +23,55 @@ document.addEventListener("click", clickEvent => {
 
 // HTML-producing component of registration events
 const registerMe = () => {
-    
+    return `
+    <div class="registrationForm">
+    <form>
+        <fieldset>
+            <label for="name">Full Name:</label>
+            <input type="text" name="fullname" autofocus placeholder="Full name" />
+        </fieldset>
+        <fieldset>
+            <label for="email">Email:</label>
+            <input type="text" name="email" autofocus placeholder="Email address" />
+        </fieldset>
+        <fieldset>
+            <label for="password">Create Password:</label>
+            <input type="password" name="password" placeholder="Create Password" />
+        </fieldset>
+    </form>
+    <button id="registerMe">Register me!</button>
+</div>
+    `
 }
+
+// event listener for button click; stores new user info with unique id in database
+document.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "registerMe") {
+        const userName = document.querySelector("input[name='fullname']").value
+        const email = document.querySelector("input[name='email']").value
+        const password = document.querySelector("input[name='password']").value
+
+        // check to see whether an account with these credentials already exists in database
+
+        const usersArray = getUsers()
+
+        for (let i = 0; i < usersArray.length; i++) {
+            if (usersArray[i].name === userName || usersArray[i].email === email) {
+                alert("User with this name and/or email already exists in database")
+                return
+            } else {
+                const newUserId = usersArray.length + 1
+                
+                let newUser = {
+                "name": userName,
+                "email": email,
+                "password": password,
+                "id": parseInt(newUserId)
+                }
+
+                sendUserToDatabase(newUser)
+                alert("Congratulations on signing up!")
+            }
+        }
+    }
+})
