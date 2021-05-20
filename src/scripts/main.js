9654456a40853e05d812071d02bddd80c3655f6d
alambrow <alexander.lambrow@gmail.com>
2021-05-20 09:32:47 -0500

    working on fetch calls on main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,13 @@
 import { GiffyGram } from "./GiffyGram.js"
 import { LoginForm } from "./auth/Login.js"
+import { fetchPosts, fetchUsers, fetchLikes } from "./data/provider.js"
 
 const applicationElement = document.querySelector(".giffygram")
 
 export const renderApp = () => {
+    fetchUsers().then(fetchPosts).then(fetchLikes)
+
     const user = parseInt(localStorage.getItem("gg_user"))
-    
     if (user) {
         applicationElement.innerHTML = GiffyGram()
     } else {
