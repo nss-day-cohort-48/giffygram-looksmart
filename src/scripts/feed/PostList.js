7ed5de43cee514e0d2e59563deaecd5bb7a52f11
wooldridge12 <wooldridgemikey@gmail.com>
2021-05-21 12:54:20 -0400

    Working on eventListeners for footer.

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -8,7 +8,7 @@ export const PostList = () => {
 
     const listPosts = posts.map(
         post => {
-            return `<li> <h2 class="gif__postTitle>${post.title}</h2> ${post.imageURL} <h5>${post.description}</h5></li>`
+            return `<li> <h2 class="gif__postTitle">${post.title}</h2> ${post.imageURL} <h5>${post.description}</h5></li>`
         })
 
 html += listPosts.join("")
