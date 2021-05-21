17007ce45cb70b71a1e0edd1d2a8dcc6988b3805
alambrow <alexander.lambrow@gmail.com>
2021-05-21 16:01:14 -0500

    got messages list displaying

--- src/scripts/friends/DirectMessage.js
+++ src/scripts/friends/DirectMessage.js
@@ -1,3 +1,5 @@
+import { getMessages } from "../data/provider.js"
+
 export const messageBox = () => {
     return `<div class="messageBox">
             </div>
@@ -5,5 +7,24 @@ export const messageBox = () => {
 }
 
 export const receivedMessages = () => {
-    return `<h1>Your DMs</h1>`
+    const messagesArray = getMessages()
+    const userId = parseInt(localStorage.getItem("gg_user"))
+    const inbox = []
+
+    for (let i = 0; i < messagesArray.length; i++) {
+        if (messagesArray[i].userId === userId) {
+            inbox.push(messagesArray[i])
+        }
+    }
+    
+    return `<h1>Your DMs</h1>
+        ${inboxList()}        
+    `
+}
+
+const inboxList = () => {
+    return `<ul>
+    <li>Message</li>
+    </ul>
+    `
 }
