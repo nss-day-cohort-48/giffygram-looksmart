26a431ece01ec539e690aa51978547f9a5ded3b1
alambrow <alexander.lambrow@gmail.com>
2021-05-24 15:57:35 -0500

    marked as read functionality implemented

--- src/scripts/friends/DirectMessage.js
+++ src/scripts/friends/DirectMessage.js
@@ -1,31 +1,62 @@
-import { getMessages, getUsers, deleteMessage } from "../data/provider.js"
+import { getMessages, getUsers, deleteMessage, markMessageAsRead } from "../data/provider.js"
 
 export const DMBox = () => {
     return `<div class="DMBox"></div>`
 }
 
+
+// TODO: add read messages list filter
 export const receivedMessages = () => {
     const messagesArray = getMessages()
     const userId = parseInt(localStorage.getItem("gg_user"))
     const inbox = []
+    const readInbox = []
 
     for (let i = 0; i < messagesArray.length; i++) {
-        if (messagesArray[i].recipientId === userId) {
+        if (messagesArray[i].recipientId === userId && messagesArray[i].read === false) {
             inbox.push(messagesArray[i])
         }
     }
     
+    for (let i = 0; i < messagesArray.length; i++) {
+        if (messagesArray[i].recipientId === userId && messagesArray[i].read === true) {
+            readInbox.push(messagesArray[i])
+        }
+    }
+
     return `<div> 
     <div class="DMTopflex">
     <h1>Your DMs</h1>
     <button id="closeMessageBox">Close</button>
     </div>
+    <h3>inbox</h3>
     ${inbox.map(inboxList).join("\n")}
+    <h3>read DMs</h3>
+    ${readInbox.map(readList).join("\n")}
     <div>
     `
 }
 
 const inboxList = (message) => {
+    const usersArray = getUsers()
+
+    let senderName = "..."
+
+    for (let i = 0; i < usersArray.length; i++) {
+        if (usersArray[i].id === message.userId) {
+            senderName = usersArray[i].name
+        }
+    }
+
+    return `
+    <div>${senderName}: ${message.text}
+    <button id="markAsRead--${message.id}">mark as read</button>
+    <button id="deleteMessage--${message.id}">delete</button>
+    </div>
+    `
+}
+
+const readList = (message) => {
     const usersArray = getUsers()
 
     let senderName = "..."
@@ -45,7 +76,14 @@ const inboxList = (message) => {
 
 document.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "closeMessageBox") {
-        document.querySelector(".giffygram").dispatchEvent(new CustomEvent("stateChanged"))
+        document.querySelector(".DMBox").innerHTML = DMBox()
+    }
+})
+
+document.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id.startsWith("markAsRead--")) {
+        const [, messageId] = clickEvent.target.id.split("--")
+        markMessageAsRead(parseInt(messageId))
     }
 })
 
