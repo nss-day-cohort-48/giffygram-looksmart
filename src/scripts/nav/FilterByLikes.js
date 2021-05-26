03a82af649e5c1a32497dfaf7371490805e43015
wooldridge12 <wooldridgemikey@gmail.com>
2021-05-26 15:34:17 -0400

    comments

--- src/scripts/nav/FilterByLikes.js
+++ src/scripts/nav/FilterByLikes.js
@@ -8,20 +8,30 @@ export const filterByLikes= () => {
     const filteredPosts = []
 
     const userId = parseInt(localStorage.getItem("gg_user"))
-    
+    //Pulling out likeId into filteredLikes (likes.userId = userId)
     for (let i = 0; i < likes.length; i++) {
         if (likes[i].userId === userId) {
             filteredLikes.push(likes[i])
         }
     }
-    for (let i = 0; i < likes.length; i++) {
-        for(let n = 0; n < posts.length; n++) {
-            if (likes[i].postId === posts[n].id) {
-                filteredPosts.push(posts[n])
+    //Pulling out postId into filteredPosts ()
+    // for (let i = 0; i < filteredLikes.length; i++) {
+    //     for(let n = 0; n < posts.length; n++) {
+    //         if (filteredLikes[i].postId === posts[n].id) {
+    //             filteredPosts.push(posts[n])
+    //         }
+    //     }
+    // }
+
+    //This for of loop works the same way as above.
+    for (const like of filteredLikes) {
+        for (const post of posts) {
+            if (like.postId === post.id) {
+                filteredPosts.push(post)
             }
         }
     }
-
+ 
     const usersArray = getUsers()
     let html = "<div id='postingWall'>"
     html += `
